c080df350f5b4546b8a5c4af9e28b0b9e6ee9b09
s21karkmark <s21karkmark@venta.lv>
2022-08-17 14:42:06 +0300

    rest of the exersices 1

--- exercise6.java
+++ exercise6.java
@@ -1,18 +1,1 @@
-import java.util.Scanner;
 
-public class exercise6 {
-    public static void main(String[] args){
-        Scanner obj = new Scanner(System.in);
-
-        System.out.printf("Enter boolean value: ");
-        boolean valueA = obj.nextBoolean();
-
-
-        if (valueA == true){
-            System.out.print("Opposite of \"true\" is \"false\"");
-        }
-        else{
-            System.out.print("Opposite of \"false\" is \"true\"");
-        }
-    }
-}
